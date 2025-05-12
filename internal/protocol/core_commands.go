a7eaee98e9ecd0d6ce0ab6db2220c2df9af15f8d
Harsh Raj <saketharshraj@gmail.com>
2025-05-13 01:28:29 +0530

    setCommand register

--- internal/protocol/core_commands.go
+++ internal/protocol/core_commands.go
@@ -1,10 +1,15 @@
 package protocol
 
-import "flex-db/internal/resp"
+import (
+	"flex-db/internal/resp"
+	"strconv"
+	"time"
+)
 
 // adds all the core commands to the registry
 func (r *CommandRegistry) registerCoreCommands() {
 	r.Register("PING", pingCommand)
+	r.Register("SET", setCommand)
 }
 
 func pingCommand(h *Handler, args []resp.Value) resp.Value {
@@ -13,4 +18,47 @@ func pingCommand(h *Handler, args []resp.Value) resp.Value {
 	}
 
 	return args[0]
+}
+
+func setCommand(h *Handler, args []resp.Value) resp.Value {
+	if len(args) < 2 {
+		return resp.NewError("ERR wrong number of arguments")
+	}
+
+	key := args[0].Str
+	value := args[1].Str
+
+	var expiry *time.Time
+
+	// now check for expiry argument
+	i := 2
+	for i < len(args) {
+		if i+1 >= len(args) {
+			break
+		}
+
+		option := args[2].Str
+		if option == "EX" {
+			seconds, err := strconv.ParseInt(args[i+1].Str, 10, 64)
+			if err != nil {
+				return resp.NewError("ERR in valid expire time in 'set' command")
+			}
+			t :=  time.Now().Add(time.Duration(seconds) * time.Second)
+			expiry = &t
+			i += 2
+		} else if option == "PX" {
+			millis, err := strconv.ParseInt(args[i+1].Str, 10, 64)
+			if err != nil {
+				return resp.NewError("ERR invalid expire time in 'set' command")
+			}
+			t := time.Now().Add(time.Duration(millis) * time.Millisecond)
+			expiry = &t
+			i += 2
+		} else {
+			break
+		}
+	}
+
+	h.DB.Set(key, value, expiry)
+	return resp.NewSimpleString("OK")
 }
